d416dbe7899c001f54c094e799b9da3ba46fbd4f
Quirin Reichmeyer <quirinreichmeyer@web.de>
2023-05-28 17:40:27 +0200

    Dropdown issues

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-// import "@hotwired/turbo-rails"
-// import "controllers"
+import "@hotwired/turbo-rails"
+import "controllers"
 // import Rails from "@rails/ujs"
 //import "jquery"
 // import "jquery-ui"
@@ -8,31 +8,12 @@
 // import "jquery-ui/ui/widgets/datepicker"
 // import './add_jquery'
 
-// document.addEventListener('turbo:load', function () {
-//   $('.ui.dropdown').dropdown();
-// });
-// Rails.start();
-// window.Rails = Rails;
-
-import { Turbo } from "@hotwired/turbo-rails"
-import 'semantic-ui-dropdown'
-
 document.addEventListener('turbo:load', function () {
-  const dropdowns = document.querySelectorAll('.ui.dropdown');
-
-  dropdowns.forEach((dropdown) => {
-    $(dropdown).dropdown();
-  });
-
-  // Reinitialize dropdowns after Turbo Frame updates
-  document.addEventListener('turbo:frame-load', function (event) {
-    const frameDropdowns = event.target.querySelectorAll('.ui.dropdown');
-    frameDropdowns.forEach((dropdown) => {
-      $(dropdown).dropdown();
-    });
-  });
+  $('.ui.dropdown').dropdown();
 });
+Rails.start();
+window.Rails = Rails;
 
-Turbo.start();
+
 
 
